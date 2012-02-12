1bd9112419d05916b338e3ef599b4e8287b19571
Rodney Folz <folz@rodneyfolz.com>
2012-02-11 22:20:16 -0800

    Clean up models.

--- djitter/djweet/models.py
+++ djitter/djweet/models.py
@@ -5,29 +5,37 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.forms import ModelForm
 
-# Profile model: basic user info and a M2MField of users they are following.
 class Profile(models.Model):
+	""" A Profile keeps track of a User's information """
+	
 	user = models.ForeignKey(User, unique=True)
+	
 	name = models.CharField(max_length=30, blank=True, default="")
+	
 	date_added = models.DateField(auto_now_add=True)
+	
 	following = models.ManyToManyField(User, related_name = "follower")
 	
 	def __unicode__(self):
 		return self.name
-# If user doesn't have a profile yet, autocreate one!
+
+# Automatically create a Profile for a User if the User doesn't have one.
 User.profile = property(lambda u: Profile.objects.get_or_create(user=u)[0])
 
 
-# Chirp model: FKey to owner, text field, and date for sorting.
 class Chirp(models.Model):
+	""" A Chirp is a status update from a User """
+	
 	user = models.ForeignKey(User, unique=True)
+	
 	text = models.CharField(max_length=140, blank=True, default="")
+	
 	date_added = models.DateField(auto_now_add=True)
 	
 	def __unicode__(self):
 		return self.user + self.date_added
-		
-		
+
+
 # Not necessary, but useful in demonstrating manager models
 class FollowManager(models.Model):
 	follower = models.ForeignKey(Profile)
