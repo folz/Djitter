167215f23f2b3b71f5095c0eb5d10957504aa880
Rodney Folz <folz@rodneyfolz.com>
2012-02-11 22:04:57 -0800

    Required? django-reg setting

--- djitter/settings.py
+++ djitter/settings.py
@@ -121,6 +121,8 @@ INSTALLED_APPS = (
     'djweet',
 )
 
+ACCOUNT_ACTIVATION_DAYS = 2
+
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error.
